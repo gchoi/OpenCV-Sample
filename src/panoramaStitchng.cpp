fb67580cef3006a730a484d8ca9e957c4a7fc8d6
ginrou799@gmail.com <ginrou799@iMac.local>
2012-06-25 08:48:35 +0900

    findRotationMatrixga

--- src/panoramaStitchng.cpp
+++ src/panoramaStitchng.cpp
@@ -33,6 +33,8 @@ private:
 };
 
 void printMat( Mat m );
+Mat findRotationMatrix( const vector<Point2f> srcPoints, const vector<Point2f>  dstPoints );
+
 
 int main( int argc, char *argv[] ){
   if( argc < 1 ) return 1;
@@ -51,7 +53,7 @@ int main( int argc, char *argv[] ){
   vector< vector<DMatch> > matches;
   for( int i = 0; i < imgs.size()-1; ++ i ){
 
-    matcher.radiusMatch( imgs[i]->desc, imgs[i+1]->desc, matches, 0.3);
+    matcher.radiusMatch( imgs[i+1]->desc, imgs[i]->desc, matches, 0.3);
 
     vector<DMatch> topMatch;
     for( int j = 0; j < matches.size() ; ++ j ){
@@ -63,8 +65,8 @@ int main( int argc, char *argv[] ){
     char filename[256];
     sprintf( filename, "matching%02d.png", i );
     Mat dst;
-    drawMatches( imgs[i]->img, imgs[i]->kpt, 
-		 imgs[i+1]->img, imgs[i+1]->kpt,
+    drawMatches( imgs[i+1]->img, imgs[i+1]->kpt, 
+		 imgs[i]->img, imgs[i]->kpt,
 		 topMatch, dst);
     imwrite( filename, dst );
 
@@ -72,18 +74,18 @@ int main( int argc, char *argv[] ){
     for( int j = 0; j < topMatch.size() ; ++j ){
       int srcIdx = topMatch[j].queryIdx;
       int dstIdx = topMatch[j].trainIdx;
-      KeyPoint srcKpt = imgs[i]->kpt[srcIdx];
-      KeyPoint dstKpt = imgs[i+1]->kpt[dstIdx];
+      KeyPoint srcKpt = imgs[i+1]->kpt[srcIdx];
+      KeyPoint dstKpt = imgs[i]->kpt[dstIdx];
       srcPoints.push_back( srcKpt.pt );
       dstPoints.push_back( dstKpt.pt );
     }
 
-    imgs[i]->homography = findHomography( srcPoints, dstPoints, CV_RANSAC, 3);
+    imgs[i]->homography = findRotationMatrix( srcPoints, dstPoints );
     printMat( imgs[i]->homography );
   }
 
   // project Image
-  int Width = imgs[0]->width * imgs.size() * 0.2;
+  int Width = imgs[0]->width * imgs.size() * 1.0;
   int Height = imgs[0]->height * 2.0;
   Mat panorama = Mat( Height, Width, CV_8UC1 );
   Mat trans = Mat::eye( 3, 3, CV_64F );
@@ -151,4 +153,49 @@ void printMat( Mat m ){
       }
       printf("\n");
     }
+}
+
+
+Mat findRotationMatrix( const vector<Point2f> srcPoints, const vector<Point2f>  dstPoints )
+{
+  double f = 1.0;
+  Mat A = Mat( srcPoints.size() * 2, 3,CV_64F);
+  Mat y = Mat( srcPoints.size() * 2, 1, CV_64F);
+
+  for( int i = 0; i < A.rows; i += 2){
+    
+    Point2f srcPt = srcPoints[i/2];
+    Point2f dstPt = dstPoints[i/2];
+    A.at<double>(0, i )	  = -srcPt.x * srcPt.y / f;
+    A.at<double>(1, i )	  = f + srcPt.x * srcPt.x  / f;
+    A.at<double>(2, i )	  = -srcPt.y;
+    A.at<double>(0, i+1 ) = -( f + srcPt.y * srcPt.y  / f );
+    A.at<double>(1, i+1 ) = srcPt.x * srcPt.y / f;
+    A.at<double>(2, i+1 ) = srcPt.x;
+    y.at<double>(0, i )   = dstPt.x - srcPt.x ;
+    y.at<double>(0, i+1 ) = dstPt.y - srcPt.y ;
+    printf("f = %lf\tsrc = %f, %f\t dst = %f, %f\n",f, srcPt.x, srcPt.y, dstPt.x, dstPt.y);
+  }
+
+  printf("putting done\n");
+
+  Mat AtA = A.t() * A;
+  printf("AtA\n");
+  printMat(AtA);
+  Mat AtAInv = AtA.inv(DECOMP_SVD);
+  printf("AtAInv\n");
+  printMat(AtAInv);
+  Mat omega = AtAInv * A.t() * y;
+  printf("omega\n");
+  printMat(omega);
+  double wx = omega.at<double>( 0, 0 );
+  double wy = omega.at<double>( 1, 0 );
+  double wz = omega.at<double>( 2, 0 );
+  Mat nCross = ( Mat_<double>(3, 3) <<
+		 0  , -wz,  wy,
+		 wz ,   0, -wx,
+		 -wy,  wx,   0);
+  return Mat::eye(3, 3, CV_64F) + nCross;
+
+
 }
